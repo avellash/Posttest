43cff72de0f32457e243f865b71e3d71e95774e6
avellash <avellash.tik@gmail.com>
2024-10-08 20:47:37 +0800

    Add files via upload

---
+++ 2409106045_Posttest5_AvellaSalsabellaH_A2'24.py
@@ -0,0 +1,18 @@
+data_peserta = 
+while True:
+    pilih = int(input("PILIH :"))
+    if pilih == 1: 
+        nama = input("Kim Sujung : ")
+        negara = input("Korea : ")
+    if pilih == 2: 
+        nama = input("Tomioka Mai : ")
+        negara = input("Jepang : ") 
+    if pilih == 3: 
+        nama = input("Ling Ling : ")
+        negara = input("Malaysia : ")
+    if pilih == 4: 
+        nama = input("Nam Yuju : ")
+        negara = input("Korea : ")
+    if pilih == 5: 
+        nama = input("Choi Jungeun : ")
+        negara = input("Korea : ")
